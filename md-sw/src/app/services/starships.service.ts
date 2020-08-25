0711f9a5252b15b5fdc23493cb9288a7cd01022d
Javier Garcia Jimenez <javier.garciaj@concatel.com>
2020-08-26 01:08:43 +0200

    Cache requests on starships.service
    **This allows us to reduce the number of server calls. In short, instead of sending a new HTTP request to the server, we do it once. The second, third and fourth time we subscribe it returns the cached value instead of doing a whole new server call.
    The publishReplay internally uses the ReplaySubject that takes as the second argument the time window which defines how long is each cached item valid.

--- md-sw/src/app/services/starships.service.ts
+++ md-sw/src/app/services/starships.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { StarShip } from '@/core/models/starship.model';
 import { Paginated } from '@/core/models/pagination.model';
 import { Observable, of } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, publishReplay, refCount, take } from 'rxjs/operators';
 import { environment } from 'environments/environment';
 
 @Injectable({
@@ -11,26 +11,42 @@ import { environment } from 'environments/environment';
 })
 export class StarShipsService {
 
+  private cacheStarShip: Observable<StarShip>[] = [];
+  private cachePage: Observable<Paginated<StarShip>>[] = [];
+
   constructor(private http: HttpClient) { }
 
   /**
    * Get all starships in paginated format
    */
   getAllStarShips(page: number = 1): Observable<Paginated<StarShip>> {
-    const endpoint = `${environment.api_sw_url_base}/starships/?page=${page}`;
-    return this.http.get<Paginated<StarShip>>(endpoint)
-      .pipe(
-        switchMap(response => {
-          return of<Paginated<StarShip>>(response);
-        })
-      );
+    if (!this.cachePage[page]) {
+      const endpoint = `${environment.api_sw_url_base}/starships/?page=${page}`;
+      this.cachePage[page] = this.http.get<Paginated<StarShip>>(endpoint)
+        .pipe(
+          publishReplay(1, 5 * 60 * 1000),
+          refCount(),
+          take(1),
+          switchMap(response => {
+            return of<Paginated<StarShip>>(response);
+          })
+        );
+    }
+    return this.cachePage[page];
   }
 
   /**
    * Get one Starships
    */
   getStarShip(id: string): Observable<StarShip> {
-    const endpoint = `${environment.api_sw_url_base}/starships/${id}/`;
-    return this.http.get<StarShip>(endpoint);
+    if (!this.cacheStarShip[id]) {
+      const endpoint = `${environment.api_sw_url_base}/starships/${id}/`;
+      this.cacheStarShip[id] = this.http.get<StarShip>(endpoint).pipe(
+        publishReplay(1, 5 * 60 * 1000),
+        refCount(),
+        take(1),
+      );
+    }
+    return this.cacheStarShip[id];
   }
 }
